6af7bdfc61ac7ef0da54b535340b4766dd9de515
lakindu hewawasam <lakindu2002@live.com>
2022-04-16 14:42:57 +0530

    added the sample user data

---
+++ data/users.js
@@ -0,0 +1,51 @@
+export const users = [
+    {
+        id: '1A',
+        name: 'Lakindu Hewawasam',
+        email: 'example@email.com',
+        username: 'lakindu',
+        age: 19
+    },
+    {
+        id: '2A',
+        name: 'Joseph Pieris',
+        email: 'joseph@email.com',
+        username: 'joseph',
+        age: 25
+    },
+    {
+        id: '3A',
+        name: 'Andre Johns',
+        email: 'andre@email.com',
+        username: 'andre',
+        age: 20
+    },
+    {
+        id: '4A',
+        name: 'Hakeem',
+        email: 'hakeem@email.com',
+        username: 'hakeem',
+        age: 40
+    },
+    {
+        id: '5A',
+        name: 'Jamal',
+        email: 'jamal@email.com',
+        username: 'jamal',
+        age: 36
+    },
+    {
+        id: '6A',
+        name: 'Robert Downey Jr',
+        email: 'downey@email.com',
+        username: 'downey',
+        age: 42
+    },
+    {
+        id: '7A',
+        name: 'Chris Hemsworth',
+        email: 'chris@email.com',
+        username: 'chris',
+        age: 30
+    }
+]
--- index.js
+++ index.js
@@ -1,26 +1,3 @@
 const express = require('express');
 const expressGraph = require('express-graphql');
 const graphQl = require('graphql');
-
-const { graphqlHTTP } = expressGraph;
-const { buildSchema } = graphQl;
-
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-
-const PORT = 4200;
-
-const app = express();
-
-app.use('/graph', graphqlHTTP({
-    graphiql: true,
-    schema
-}));
-
-app.listen(PORT, () => {
-    console.log(`GraphQL API is Up and Running on PORT ${PORT}`);
-})
