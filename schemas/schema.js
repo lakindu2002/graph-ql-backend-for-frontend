13b0a3f7cccd946d20387cfbf0e94e2a8937b99c
lakindu hewawasam <lakindu2002@live.com>
2022-04-16 16:13:47 +0530

    added the comments for the schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,6 +1,7 @@
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList } = require('graphql');
 const { getAllUsers, getUserById, getUserByUsername } = require('../resolvers/user-resolver');
 
+// define the schema (provide information about the fields the query can return)
 const UserType = new GraphQLObjectType({
     name: 'user',
     fields: {
@@ -22,19 +23,19 @@ const UserType = new GraphQLObjectType({
     }
 })
 
-
+// declare the queries with the resolvers
 const RootQueries = new GraphQLObjectType({
     name: 'rootQuery',
     fields: {
         getAllUsers: {
-            type: new GraphQLList(UserType),
+            type: new GraphQLList(UserType), // return type
             resolve: () => {
                 return getAllUsers();
             }
         },
         getUserById: {
             type: UserType,
-            args: {
+            args: { // input args
                 userId: {
                     type: GraphQLString
                 }
